02aeb340fd7bb62adf2ed6873a2fb9de6d323d79
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-08-19 09:26:51 +0200

    Füge UpdateJobRequest für die Validierung von Stellenanzeigen-Updates hinzu

---
+++ Stellenanzeige/app/Http/Requests/UpdateJobRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateJobRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|required|string|max:255',
+            'company' => 'sometimes|required|string|max:255',
+            'location' => 'sometimes|nullable|string|max:255',
+            'description' => 'sometimes|required|string',
+            'type' => 'sometimes|required|in:full-time,part-time,contract,internship',
+            'salary' => 'sometimes|nullable|integer|min:0',
+            'published_at' => 'sometimes|nullable|date',
+        ];
+    }
+}
