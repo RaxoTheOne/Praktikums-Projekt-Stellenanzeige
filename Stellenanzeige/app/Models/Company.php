2d2d1628aee37932f2ba3f242e04c648e79d2779
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2025-08-19 09:27:11 +0200

    Füge Company-Modell hinzu, um Unternehmensdaten zu verwalten

---
+++ Stellenanzeige/app/Models/Company.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    /** @use HasFactory<\Database\Factories\CompanyFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'website',
+        'email',
+        'phone',
+    ];
+
+    public function jobListings()
+    {
+        return $this->hasMany(JobListing::class);
+    }
+}
